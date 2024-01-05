95328500b400d468b66fdacae2da09b604a6f225
hyojinkim2028 <12hyojinkim14@gmail.com>
2024-01-05 17:06:30 +0900

    'init'

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,8 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private configService : ConfigService
-  ){}
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return `Hello World ${this.configService.get('SERVER_PORT')}`!;
+    return `Hello Hello World ${this.configService.get('SERVER_PORT')}`!;
   }
 }
