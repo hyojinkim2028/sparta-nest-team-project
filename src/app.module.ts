56bed1a5e312d6f6d943b63b2f0b37ee9b4457f3
hyojinkim2028 <12hyojinkim14@gmail.com>
2024-01-05 16:59:30 +0900

    fix:(main) 프리티어 오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configModuleValidationSchema } from './configs/env-validation.config';
+import { typeOrmModuleAsyncOptions } from './configs/database.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, validationSchema: configModuleValidationSchema
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
