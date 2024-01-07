63ec4b8e66d1b32605aff9e7ab1a02c5c4ef0362
leesin1040 <leesin1040@gmail.com>
2024-01-07 23:27:10 +0900

    fix:(User)user모듈에 jwt서비스 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,24 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '12h',
+        },
+      }),
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
