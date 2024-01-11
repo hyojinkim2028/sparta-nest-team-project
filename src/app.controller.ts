4f817fea616e4fabf29dba1b8f8abe66ba17568b
leesin1040 <leesin1040@gmail.com>
2024-01-11 12:15:52 +0900

    update:(front)원기옥 준비자세

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -77,8 +77,7 @@ export class AppController {
   async GetUserInfo(@UserInfo() user: User) {
     return {
       user,
-      message:
-        '평범한 회원가입페이지입니다. 평범하지 않은 모달이 될수도 있습니다. 로그인도 마찬가지',
+      message: '회원가입 페이지',
     };
   }
 
@@ -88,7 +87,7 @@ export class AppController {
   async boardInfo(@UserInfo() user: User, @Req() req) {
     return {
       user,
-      message: '보드상세!!',
+      message: '보드상세',
     };
   }
 
@@ -97,8 +96,7 @@ export class AppController {
   @Page('createBoardModal')
   async createBoardModal(@UserInfo() user: User, @Req() req) {
     return {
-      message:
-        '평범한 회원가입페이지입니다. 평범하지 않은 모달이 될수도 있습니다.',
+      message: '보드 모달',
     };
   }
 }
